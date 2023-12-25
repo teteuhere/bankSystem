ce7a6659a9b17bb16c4b6113b56e01ad1dff69e6
teteuhere <matheuss.cirq@outlook.com>
2023-12-25 20:41:08 -0300

    Update of class and parts of the code

--- src/App.java
+++ src/App.java
@@ -1,20 +1,21 @@
 public class App {
     public static void main(String[] args) {
-        
-        String sinopse;
-        sinopse = "Filme em que todos morrem por conta de que mataram o cachorro dele";
-        
         int year = 2022;
+
+        String sinopse;
+        sinopse = """
+                    Movie which John wick, a retired assassin return to kill everyone who killed his dog.
+                    Star of the show keanu reeves.
+                    Year of release: """ + year;
         
-        boolean planIncluse = false;
+        boolean plans = false;
         
         double review = 8.5;
-        double media =(9.8 + 7.5 + 8.3)/3;
+        double reviewAll =(9.8 + 7.5 + 8.3)/3;
         
-        System.out.println("Welcome for screenmatch");
-        System.out.println("Filme: John Wick 4");
+        System.out.println("Welcome for Screenmatch");
+        System.out.println("Movie: John Wick 4");
         System.out.println(sinopse);
-        System.out.println("Year of launching:  " + year);
-        System.out.println(media);
+        System.out.println(String.format("%.1f",reviewAll));
     }
 }

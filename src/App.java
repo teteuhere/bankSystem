8f577c58b0d407e9c9a0b28f051c5a81de809a13
teteuhere <matheuss.cirq@outlook.com>
2023-12-25 20:01:18 -0300

    Work in progress

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Seja bem vindo");
+        System.out.println("Welcome for screenmatch");
+        System.out.println("Filme: John Wick 4");
+        
+        String sinopse;
+        sinopse = "Filme em que todos morrem por conta de que mataram o cachorro dele";
+        System.out.println(sinopse);
+
+        int year = 2022;
+        System.out.println("Year of launching:  " + year);
+        
+        boolean planIncluse = false;
+        
+        double review = 8.5;
+        double media =(9.8 + 7.5 + 8.3)/3;
+        System.out.println(media);
+
     }
 }

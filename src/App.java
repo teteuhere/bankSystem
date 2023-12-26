3b314339b60cdb43eb5130edfa9f50d1b467fbef
teteuhere <matheuss.cirq@outlook.com>
2023-12-25 22:27:17 -0300

    Formating

--- src/App.java
+++ src/App.java
@@ -7,20 +7,18 @@ public class App {
         boolean plans = false;
         double review = 8.5;
         double reviewAll =(9.8 + 7.5 + 8.3)/3;
-        double classification;
-        classification = (int)(reviewAll/2);
+        double classification = (reviewAll/2);
 
         //Main quest
-        String sinopse;
-        sinopse = """
-                    - Welcome for Screenmatch -
+        String sinopse = """
+                                - Welcome for Screenmatch -
 
                         Movie: %s
                         Sinopse: John wick is a retired assassin return to kill everyone who killed his dog.
                         Star of the show keanu reeves.
                         Year of release: %s
                         Reviews: %.1f
-                        Classification: %s """.formatted(movie, year, reviewAll, classification);
+                        Classification: %.0f """.formatted(movie, year, reviewAll, classification);
         
         System.out.println(sinopse);
     }
